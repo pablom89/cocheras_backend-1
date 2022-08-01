9dbd6675c795d8e79226eddb1c6d45c53eb48bb9
César Muzio <cmuzio375@gmail.com>
2022-08-01 12:23:48 -0300

    agregué el verify file al index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,15 @@
 const  validar_campos  = require('./validar-campos');
 const  validar_jwt  = require('./validar-jwt');
-const validar_rol = require('./validar-rol')
-const validar_archivos = require('./validar-archivo')
+const validar_rol = require('./validar-rol');
+const validar_archivos = require('./validar-archivo');
+const upload = require('./multer');
+const verify_file = require('./verifyFile')
 
 module.exports = {
     ...validar_campos,
     ...validar_jwt,
     ...validar_rol,
-    ...validar_archivos
+    ...validar_archivos,
+    ...upload,
+    ...verify_file
 }
