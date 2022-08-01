4bf78302a90dee997746b023e361dc801bd84126
Pablo Muzio <pablo_89_cba@hotmail.com>
2022-07-31 23:41:25 -0300

    Cambiando cors

--- models/server.js
+++ models/server.js
@@ -1,68 +1,57 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const dbConnection = require('../db/config');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const dbConnection = require("../db/config");
 
+class Server {
+  constructor() {
+    this.app = express();
 
+    this.paths = {
+      auth: "/api/auth",
+      cocheras: "/api/cocheras",
+      user: "/api/user",
+      vehiculos: "/api/vehiculos",
+    };
 
+    this.conectarDB();
+    this.middlewares();
+    this.routes();
 
+    this.corsOptions = {
+      origin: "*",
+      optionsSuccessStatus: 200,
+    };
+  }
 
-class Server{
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    constructor(){
+  middlewares() {
+    // cors
+    this.app.use(cors(this.corsOptions));
 
-        this.app = express();
+    // lectura y parseo del body
+    this.app.use(express.json());
 
-        this.paths = {
-            auth: '/api/auth',
-            cocheras:'/api/cocheras',
-            user: '/api/user',
-            vehiculos:'/api/vehiculos'
-        }
-        
-        
-        this.conectarDB()
-        this.middlewares()
-        this.routes()
-        
-        this.corsOptions = {
-            origin: 'https://parking-now.herokuapp.com',
-            optionsSuccessStatus: 200 
-        }
-    }
-    
-    async conectarDB(){
-        await dbConnection()
-    }
-    
-    middlewares(){
-        
-        // cors
-        this.app.use( cors(this.corsOptions))
-        
-        // lectura y parseo del body
-        this.app.use( express.json() )
-        
-        
-        // servir carpeta pública , construir path
+    // servir carpeta pública , construir path
 
-        /* this.app.use( express.static('public')) */
-    }
+    /* this.app.use( express.static('public')) */
+  }
 
-    routes(){
+  routes() {
+    this.app.use(this.paths.auth, require("../routes/auth"));
+    this.app.use(this.paths.user, require("../routes/user"));
+    this.app.use(this.paths.cocheras, require("../routes/cocheras"));
+    this.app.use(this.paths.vehiculos, require("../routes/vehiculos"));
+  }
 
-        this.app.use( this.paths.auth, require('../routes/auth') )
-        this.app.use( this.paths.user, require('../routes/user') )
-        this.app.use( this.paths.cocheras, require('../routes/cocheras') )
-        this.app.use( this.paths.vehiculos, require('../routes/vehiculos') )
-    }
-
-    listen(){
-        this.app.listen( process.env.PORT, () => {
-            console.log( 'server on port : ' , process.env.PORT )
-        })
-    }
-
+  listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log("server on port : ", process.env.PORT);
+    });
+  }
 }
 
 module.exports = Server;
